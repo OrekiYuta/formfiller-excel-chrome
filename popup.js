c9acd9ec4a4f950fb0705d72864db8237c6e00e8
Orekiyuta <orekiyuta@gmail.com>
2025-08-03 01:21:39 +0800

    build: release 0.0.1

--- popup.js
+++ popup.js
@@ -1,3 +1,1 @@
-document.getElementById('openOptions').addEventListener('click', () => {
-  chrome.runtime.openOptionsPage();
-});
+document.getElementById("openOptions").addEventListener("click",()=>{chrome.runtime.openOptionsPage()});
